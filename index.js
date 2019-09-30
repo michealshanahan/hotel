23e4564c6c093b9967d28d3d46d25100a6e28cc4
Micheal Shanahan <m_shanahan@hotmail.com>
2019-09-30 10:38:35 -0600

    Update

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const expressJwt = require("express-jwt")
+require('dotenv').config()
+const port = process.env.port || 5566
+const path = require('path')
+
+
+
+mongoose.connect(process.env.MONGOLAB_GOLD_URI || 'mongodb://localhost:27017/hotel', {useNewUrlParser: true}).then(() => {
+    console.log('connected to mongo')
+})
+.catch(err => {
+    console.log(err)
+})
+ 
+
+app.use('/', express.json())
+
+app.use('/users', require('./routers/UsersRouter'))
+app.use('/api', expressJwt({ secret: process.env.SECRET || "just some words for local"}))
+app.use('/api/reservations', require('./routers/ReservationsRouter'))
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
+    return res.send({ message: err.message })
+})
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
+app.listen(port, () => {
+    console.log(`app is listening ${port}`)
+})
+
